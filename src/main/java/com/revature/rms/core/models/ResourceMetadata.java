63bccac3ebc0addaa2ba6e24db3ee2b6344a5dce
Wezley <wezley.singleton@gmail.com>
2020-03-23 15:43:44 -0400

    cleanup and added docs

--- src/main/java/com/revature/rms/core/models/ResourceMetadata.java
+++ src/main/java/com/revature/rms/core/models/ResourceMetadata.java
@@ -2,13 +2,28 @@ package com.revature.rms.core.models;
 
 import java.time.LocalDateTime;
 
+/**
+ * A model which is embedded into all Resource documents and is used to convey
+ * system information.
+ */
 public class ResourceMetadata {
 
+    /** The id of the creator of this resource */
     private String resourceCreatorId;
+
+    /** The id of the last modifier of this resource */
     private String lastModifierId;
+
+    /** The id of the owner of this resource */
     private String resourceOwnerId;
+
+    /** A bool representing whether or not this an active resource or not */
     private boolean isActive;
+
+    /** The datetime of resource creation */
     private LocalDateTime resourceCreationDateTime;
+
+    /** The datetime of the last modification to this resource */
     private LocalDateTime lastModifiedDateTime;
 
     public ResourceMetadata() {
@@ -20,8 +35,8 @@ public class ResourceMetadata {
      * last modifier id, and the resource creation time and last modified time is set to
      * the time of instantiation.
      *
-     * @param creator
-     * @param owner
+     * @param creator id of the user that created a resource (also used as the last modifier id)
+     * @param owner id of the owner of a resource
      */
     public ResourceMetadata(String creator, String owner) {
         this.resourceCreatorId = creator;
@@ -32,13 +47,35 @@ public class ResourceMetadata {
         this.isActive = true;
     }
 
-    public ResourceMetadata(String creatorId, LocalDateTime creationDT, String modId, LocalDateTime modDT, String ownerId, boolean active) {
-        this.resourceCreatorId = creatorId;
+    /**
+     * Creates a new ResourceMetadata instance where the provided creator id is used as the
+     * last modifier id, and the resource creation time and last modified time is set to
+     * the time of instantiation.
+     *
+     * @param creator id of the user that created a resource (also used as the last modifier id)
+     * @param owner id of the owner of a resource
+     * @param active bool representing whether or not this an active resource or not
+     */
+    public ResourceMetadata(String creator, String owner, boolean active) {
+        this(creator, owner);
+        this.isActive = false;
+    }
+
+    /**
+     * Creates a new ResourceMetadata instance.
+     *
+     * @param creator id of the user that created a resource
+     * @param creationDT datetime of resource creation
+     * @param modId id of the user that last modified this resource
+     * @param modDT datetime of the last modification to this resource
+     * @param owner id of the owner of a resource
+     * @param active bool representing whether or not this an active resource or not
+     */
+    public ResourceMetadata(String creator, LocalDateTime creationDT, String modId, LocalDateTime modDT, String owner, boolean active) {
+        this(creator,  owner, active);
         this.resourceCreationDateTime = creationDT;
         this.lastModifierId = modId;
         this.lastModifiedDateTime = modDT;
-        this.resourceOwnerId = ownerId;
-        this.isActive = active;
     }
 
     public String getResourceCreatorId() {
