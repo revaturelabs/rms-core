ba4b999436277ef7dca42ffde512a16a4a6b88b6
wezley <wezley.singleton@gmail.com>
2020-06-29 13:47:47 -0400

    changed findMyResources to findResourcesByOwnerId; adding implementation for findAll(params)

--- src/main/java/com/revature/rms/core/services/ResourceService.java
+++ src/main/java/com/revature/rms/core/services/ResourceService.java
@@ -3,9 +3,7 @@ package com.revature.rms.core.services;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
-import com.revature.rms.core.exceptions.NoImplementationException;
-import com.revature.rms.core.exceptions.ResourcePersistenceException;
-import com.revature.rms.core.exceptions.ResourceRetrievalException;
+import com.revature.rms.core.exceptions.*;
 import com.revature.rms.core.models.Resource;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
@@ -13,7 +11,13 @@ import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Serves as a base implementation for business service classes that handle resources
@@ -43,14 +47,40 @@ public abstract class ResourceService<T extends Resource> {
 
     }
 
-    public Flux<T> findAll(MultiValueMap<String, String> parameters) {
+    public Flux<? extends Resource> findAll(MultiValueMap<String, String> parameters) {
 
-        // TODO findAll with params implementation
-        return Flux.error(NoImplementationException::new);
+        List<String> paramKeys = new ArrayList<>(parameters.keySet());
+        List<String> resourceFieldNames = Stream.concat(
+                                                        Arrays.stream(resourceType.getDeclaredFields()),
+                                                        Arrays.stream(Resource.class.getDeclaredFields())
+                                                        )
+                                                .map(Field::getName)
+                                                .collect(Collectors.toList());
+
+
+        for (String paramKey : paramKeys) {
+
+            System.out.println("[DEBUG] - paramKey: " + paramKey);
+
+            if (!resourceFieldNames.contains(paramKey)) {
+                String msg = "The field, " + paramKey + ", was not found on resource type, " + resourceType.getName();
+                return Flux.error(new InvalidRequestException(msg));
+            }
+
+            String keyValue = parameters.getFirst(paramKey);
+            System.out.println("[DEBUG] - Provided key value: " + keyValue);
+            return mongoTemplate.find(query(where(paramKey).is(keyValue)), resourceType)
+                                .switchIfEmpty(Mono.error(ResourceRetrievalException::new));
+        }
+
+
+
+
+        return Flux.error(InternalServerException::new);
 
     }
 
-    public Flux<? extends Resource> findMyResources(String resourceOwnerId) {
+    public Flux<? extends Resource> findResourcesByOwnerId(String resourceOwnerId) {
         return mongoTemplate.find(query(where("metadata.resourceOwnerId").is(resourceOwnerId)), resourceType)
                 .switchIfEmpty(Flux.error(ResourceRetrievalException::new));
 
